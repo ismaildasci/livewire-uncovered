e325bbea42f55aed0c9dabc87f0948b145da4898
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-07-02 13:33:50 +0300

    starting-from-stratch

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
-
-class Counter extends Component
+class Counter
 {
     public $count = 0;
 
@@ -12,13 +10,15 @@ class Counter extends Component
     {
         $this->count++;
     }
+
     public function render()
     {
-            return <<<'HTML'
-        <div class="counter">
-            <span>{{$count}}</span>
-            <button wire:click="increment">+</button>
-        </div>
+        return <<<'HTML'
+            <div class="counter">
+                <span>{{ $count }}</span>
+
+                <button wire:click="increment">+</button>
+            </div>
         HTML;
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,27 @@
 <html>
-<link rel="stylesheet"  href="/app.css">
-<script src="/app.js"></script>
+    <link href="/app.css" rel="stylesheet">
 
-<livewire:counter />
-
-
-@livewireScripts
+    {!! livewire(App\Http\Livewire\Counter::class) !!}
 </html>
+<?php
+
+function livewire($class) {
+    $component = new $class;
+
+    return Blade::render(
+        $component->render(),
+        getProperties($component)
+    );
+}
+
+function getProperties($component) {
+    $properties = [];
+
+    $reflectedProperties = (new ReflectionClass($component))->getProperties(ReflectionProperty::IS_PUBLIC);
+
+    foreach ($reflectedProperties as $property) {
+        $properties[$property->getName()] = $property->getValue($component);
+    }
+
+    return $properties;
+}
