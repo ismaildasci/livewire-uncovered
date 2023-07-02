8dffd1811033cd30bad52a41e6fab09df3e74fa2
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-07-02 13:49:32 +0300

    refactoring-our-work

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Blade::directive('livewire', function ($expression) {
+    return "<?php echo (new App\Livewire)->initialRender({$expression}); ?>";
+});
